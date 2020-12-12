6b8204fe02fe366b0501eda0b36b1193ccad2de9
vp-gas <75901483+vp-gas@users.noreply.github.com>
2020-12-12 19:08:13 +0300

    Update Home_assignement_1_1.py

--- Home_assignement_1_1.py
+++ Home_assignement_1_1.py
@@ -3,3 +3,5 @@ hours = time // 3600
 minutes = (time - hours * 3600) // 60
 seconds = time - (hours * 3600 + minutes * 60)
 print(f"Время в формате чч:мм:сс   {hours} : {minutes} : {seconds}")
+
+''' edited '''
